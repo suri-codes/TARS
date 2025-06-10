1fdd04768cbc37face3c391074469cddee4422c7
Surendra Jammishetti <suri312006@gmail.com>
2025-06-10 12:55:33 -0700

    finished group handler

--- tars-daemon/src/handlers/group_handler.rs
+++ tars-daemon/src/handlers/group_handler.rs
@@ -1,18 +1,30 @@
-use axum::{Extension, Json, Router, routing::post};
+use axum::{
+    Extension, Json, Router,
+    routing::{get, post},
+};
 use common::{
     TarsError,
     types::{Group, Id, Name},
 };
 use sqlx::{Pool, Sqlite};
 
+/// Takes in a router and appends all handlers related to groups
 pub fn add_group_handlers(router: Router) -> Router {
     router
+        .route("/group", get(fetch_groups))
         .route("/group/create", post(create_group))
-        .route("/group/search", post(fetch_groups))
         .route("/group/update", post(update_group))
         .route("/group/delete", post(delete_group))
 }
 
+/// Takes in a `Group` and then writes that group to the database.
+///
+/// # Errors
+/// TarsError
+///
+/// This function will return an error if
+/// + Something goes wrong with sqlx.
+/// + Something goes wrong turning what sqlx returns into our wrapper types.
 async fn create_group(
     Extension(pool): Extension<Pool<Sqlite>>,
     Json(group): Json<Group>,
@@ -38,9 +50,17 @@ async fn create_group(
 
     Ok(Json(group))
 }
+
+/// Fetches all groups from the database.
+///
+/// # Errors
+/// TarsError
+///
+/// This function will return an error if
+/// + Something goes wrong with sqlx.
+/// + Something goes wrong turning what sqlx returns into our wrapper types.
 async fn fetch_groups(
     Extension(pool): Extension<Pool<Sqlite>>,
-    // Json(group): Json<Group>,
 ) -> Result<Json<Vec<Group>>, TarsError> {
     let groups = sqlx::query_as!(
         Group,
@@ -56,16 +76,70 @@ async fn fetch_groups(
 
     Ok(Json::from(groups))
 }
+
+/// Takes in a `Group` and then updates that group to the database.
+///
+/// # Errors
+/// TarsError
+///
+/// This function will return an error if
+/// + Something goes wrong with sqlx.
+/// + Something goes wrong turning what sqlx returns into our wrapper types.
 async fn update_group(
     Extension(pool): Extension<Pool<Sqlite>>,
     Json(group): Json<Group>,
 ) -> Result<Json<Group>, TarsError> {
-    // uh, how do we do this lmao, i think groups need to have an id no?
-    todo!()
+    let updated = sqlx::query_as!(
+        Group,
+        r#"
+            UPDATE Groups
+            SET
+            name = ?
+            WHERE pub_id = ?
+            RETURNING
+                name as "name: Name",
+                pub_id as "id: Id"
+
+        "#,
+        *group.name,
+        *group.id
+    )
+    .fetch_one(&pool)
+    .await?;
+
+    assert_eq!(group, updated);
+
+    Ok(Json::from(updated))
 }
+
+/// Takes in a `Group` and then deletes that group in the database.
+///
+/// # Errors
+/// TarsError
+///
+/// This function will return an error if
+/// + Something goes wrong with sqlx.
+/// + Something goes wrong turning what sqlx returns into our wrapper types.
 async fn delete_group(
     Extension(pool): Extension<Pool<Sqlite>>,
     Json(group): Json<Group>,
 ) -> Result<Json<Group>, TarsError> {
-    todo!()
+    let deleted = sqlx::query_as!(
+        Group,
+        r#"
+            DELETE FROM Groups
+            WHERE pub_id = ?
+            RETURNING
+                pub_id as "id: Id",
+                name as "name: Name"
+           
+        "#,
+        *group.id,
+    )
+    .fetch_one(&pool)
+    .await?;
+
+    assert_eq!(group, deleted);
+
+    Ok(Json::from(deleted))
 }
