3cf074f2de0bd2b1a8a04a74221803117f3bbe3b
Surendra Jammishetti <suri312006@gmail.com>
2025-06-11 10:25:37 -0700

    moved everything into daemon struct

---
+++ tars-daemon/src/daemon.rs
@@ -0,0 +1,84 @@
+use std::{fs::create_dir_all, str::FromStr};
+
+use axum::{Extension, Router, routing::get};
+use common::dirs::get_data_dir;
+use sqlx::{
+    Pool, Sqlite, SqlitePool,
+    sqlite::{SqliteConnectOptions, SqliteJournalMode},
+};
+use tokio::net::TcpListener;
+use tracing::{error, info};
+
+use crate::handlers::{add_group_handlers, add_task_handlers};
+
+pub struct TarsDaemon {
+    app: Router,
+}
+
+impl TarsDaemon {
+    pub async fn init(db_url: &str) -> Self {
+        let app = Router::new()
+            .route("/", get(root))
+            .layer(Extension(create_pool(db_url).await));
+
+        let app = add_task_handlers(app);
+        let app = add_group_handlers(app);
+
+        Self { app }
+    }
+
+    pub async fn run(self, addr: &str) {
+        let listener = TcpListener::bind(addr).await.unwrap();
+
+        info!("App lisening on {}", addr);
+
+        if let Err(e) = axum::serve(listener, self.app).await {
+            error!("{e}");
+            panic!("{e}")
+        };
+    }
+}
+
+async fn root() -> &'static str {
+    "ligma nuts pal"
+}
+
+async fn create_pool(db_url: &str) -> Pool<Sqlite> {
+    let mut data_dir = get_data_dir();
+
+    let _ = create_dir_all(&data_dir);
+
+    data_dir.push(db_url);
+
+    let db_path = data_dir;
+
+    let full_path = format!(
+        "sqlite://{}",
+        db_path
+            .to_str()
+            .expect("Database Path should be a valid string.")
+    );
+
+    let sqlite_opts = {
+        SqliteConnectOptions::from_str(&full_path)
+            .inspect_err(|e| {
+                error!(
+                    "Failed to connect to sqlite db at url: {}. error: {}",
+                    full_path, e
+                )
+            })
+            .unwrap()
+            .create_if_missing(true)
+            .journal_mode(SqliteJournalMode::Wal)
+    };
+
+    SqlitePool::connect_with(sqlite_opts.clone())
+        .await
+        .inspect_err(|e| {
+            error!(
+                "Failed to create sqlite pool. Connection Options: {:?}. Error: {}",
+                sqlite_opts, e
+            )
+        })
+        .unwrap()
+}
--- tars-daemon/src/main.rs
+++ tars-daemon/src/main.rs
@@ -1,19 +1,8 @@
-use axum::{
-    Extension, Router,
-    routing::{get, post},
-};
-use common::{DAEMON_ADDR, dirs::get_data_dir};
-use handlers::{add_group_handlers, add_task_handlers};
-use sqlx::{
-    Pool, Sqlite, SqlitePool,
-    sqlite::{SqliteConnectOptions, SqliteJournalMode},
-};
-use std::{fs::create_dir_all, str::FromStr};
-use tokio::net::TcpListener;
-use tracing::{error, info};
+use common::DAEMON_ADDR;
+use daemon::TarsDaemon;
 
+mod daemon;
 mod handlers;
-
 const DB_FILE_NAME: &str = "tars.db";
 
 #[tokio::main]
@@ -22,57 +11,7 @@ async fn main() {
     // TODO: need to integrate a daemon log file aswell
 
     //TODO: also create a notifier thread later
+    let daemon = TarsDaemon::init(DB_FILE_NAME).await;
 
-    let app = Router::new()
-        .route("/", get(root))
-        .layer(Extension(create_pool().await));
-
-    let app = add_task_handlers(app);
-    let app = add_group_handlers(app);
-
-    let listener = TcpListener::bind(DAEMON_ADDR).await.unwrap();
-
-    info!("App lisening on {}", DAEMON_ADDR);
-
-    if let Err(e) = axum::serve(listener, app).await {
-        error!("{e}");
-        panic!("{e}")
-    };
-}
-
-async fn root() -> &'static str {
-    "ligma nuts pal"
-}
-
-async fn create_pool() -> Pool<Sqlite> {
-    let mut data_dir = get_data_dir();
-
-    let _ = create_dir_all(&data_dir);
-
-    data_dir.push(DB_FILE_NAME);
-
-    let db_path = data_dir;
-
-    let full_path = format!(
-        "sqlite://{}",
-        db_path
-            .to_str()
-            .expect("Database Path should be a valid string.")
-    );
-
-    let sqlite_opts = SqliteConnectOptions::from_str(&full_path)
-        .expect("failed")
-        .create_if_missing(true)
-        .journal_mode(SqliteJournalMode::Wal);
-
-    let Ok(pool) = SqlitePool::connect_with(sqlite_opts.clone()).await else {
-        let msg = format!(
-            "Failed to create sqlite pool. Connection Options: {:?}",
-            sqlite_opts,
-        );
-        error!("{msg}");
-        panic!("{msg}")
-    };
-
-    pool
+    daemon.run(DAEMON_ADDR).await;
 }
