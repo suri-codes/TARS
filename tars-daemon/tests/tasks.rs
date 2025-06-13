ff69d4b5534c1c3e063dd297e8016ac101feba75
Surendra Jammishetti <suri312006@gmail.com>
2025-06-12 22:13:38 -0700

    commented out tests

--- tars-daemon/tests/tasks.rs
+++ tars-daemon/tests/tasks.rs
@@ -5,15 +5,15 @@ mod utils;
 
 #[tokio::test]
 async fn task_tests() {
-    let (d, addr) = new_test_daemon().await;
-    let _x = tokio::spawn(async move {
-        d.run().await;
-    });
+    // let (d, addr) = new_test_daemon().await;
+    // let _x = tokio::spawn(async move {
+    //     d.run().await;
+    // });
 
-    // now we can start using the client to test?
-    let _client = TarsClient::new(addr)
-        .await
-        .expect("failed to instantiate client");
+    // // now we can start using the client to test?
+    // let _client = TarsClient::new(addr)
+    //     .await
+    //     .expect("failed to instantiate client");
 
     //TODO: finish up the client implementation so we can actually write these tests
 }
