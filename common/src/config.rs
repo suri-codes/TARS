f9a34bb7753444f843d11a2881f62d791dad9242
Surendra Jammishetti <suri312006@gmail.com>
2025-10-15 15:36:17 -0700

    fixed config diw

--- common/src/config.rs
+++ common/src/config.rs
@@ -1,4 +1,8 @@
-use std::{env, fs, path::PathBuf};
+use std::{
+    env::{self, home_dir},
+    fs,
+    path::PathBuf,
+};
 
 use directories::ProjectDirs;
 use lazy_static::lazy_static;
@@ -63,12 +67,25 @@ pub fn get_data_dir() -> PathBuf {
 
 pub fn get_config_dir() -> PathBuf {
     if let Some(s) = CONFIG_FOLDER.clone() {
-        s
-    } else if let Some(proj_dirs) = project_directory() {
-        proj_dirs.config_local_dir().to_path_buf()
-    } else {
-        PathBuf::from(".").join(".config")
+        return s;
     }
+
+    #[cfg(target_os = "macos")]
+    if let Some(home) = home_dir() {
+        let macos_preferred = home.join(".config").join("tars");
+        return macos_preferred;
+    }
+
+    if let Some(home) = home_dir() {
+        let generic = home.join(".config");
+        return generic;
+    }
+
+    if let Some(proj_dirs) = project_directory() {
+        return proj_dirs.config_local_dir().to_path_buf();
+    }
+
+    PathBuf::from(".").join(".config")
 }
 
 fn project_directory() -> Option<ProjectDirs> {
